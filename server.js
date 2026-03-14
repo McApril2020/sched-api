e3043ac58b0be5d89482eb53f96b32dfd8db319b
McApril2020 <67496962+McApril2020@users.noreply.github.com>
2026-03-14 21:24:10 +0800

    updating port

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const faculty_sched = require('./data/faculty_sched');
 const students = require('./data/students');
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 const SECRET_KEY = process.env.JWT_SECRET;
 const jwt = require("jsonwebtoken");
