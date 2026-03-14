6d69cdbbdc3691c47b4f67bcd7385663d27c29ff
McApril2020 <67496962+McApril2020@users.noreply.github.com>
2026-03-14 23:07:28 +0800

    new sched

--- data/faculty_sched.js
+++ data/faculty_sched.js
@@ -1,6 +1,6 @@
 const faculty_sched = [
     {facultyID: 1, subject: 'Mathematics', sched: '2026-03-14', timeStart: '21:00', timeEnd: '22:00'},
-    {facultyID: 1, subject: 'Science', sched: '2026-03-14', timeStart: '22:00', timeEnd: '23:00'},
+    {facultyID: 1, subject: 'Science', sched: '2026-03-14', timeStart: '23:00', timeEnd: '23:59'},
     {facultyID: 1, subject: 'English', sched: '2026-03-16', timeStart: '10:00', timeEnd: '11:00'},
     {facultyID: 1, subject: 'History', sched: '2026-03-16', timeStart: '11:00', timeEnd: '12:00'},
     // {facultyID: 1, subject: 'Physical Ed', sched: '2026-03-16', timeStart: '13:00', timeEnd: '14:00'},
