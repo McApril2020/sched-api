3691a4a76a6823fb0420537bcab44ced1fd06ab2
McApril2020 <67496962+McApril2020@users.noreply.github.com>
2026-03-15 21:04:14 +0800

    added student and faculty sched

--- data/students.js
+++ data/students.js
@@ -1,11 +1,15 @@
 const students = [
-    { id: 1, username: '2026001', fullName: 'Jessica Alate', subject: 'Mathematics' , sched: '2026-03-16', timeStart: '08:00', timeEnd: '09:00'},
-    { id: 2, username: '2026002', fullName: 'Marlon Brant', subject: 'Mathematics' , sched: '2026-03-14', timeStart: '21:00', timeEnd: '22:00'},
-    { id: 3, username: '2026003', fullName: 'Albert Concio', subject: 'Mathematics' , sched: '2026-03-14', timeStart: '21:00', timeEnd: '22:00'},
+    { id: 1, username: '2026001', fullName: 'Jessica Alate', subject: 'Mathematics' , sched: '2026-03-15', timeStart: '21:00', timeEnd: '22:00'},
+    { id: 2, username: '2026002', fullName: 'Marlon Brant', subject: 'Mathematics' , sched: '2026-03-15', timeStart: '21:00', timeEnd: '22:00'},
+    { id: 3, username: '2026003', fullName: 'Albert Concio', subject: 'Mathematics' , sched: '2026-03-15', timeStart: '21:00', timeEnd: '22:00'},
     { id: 4, username: '2026004', fullName: 'James Smith', subject: 'Computer Science' , sched: '2026-03-14', timeStart: '21:00', timeEnd: '22:00'},
     { id: 5, username: '2026001', fullName: 'Jessica Alate', subject: 'Science' , sched: '2026-03-16', timeStart: '09:00', timeEnd: '10:00'},
-    { id: 6, username: '2026003', fullName: 'Albert Concio', subject: 'English' , sched: '2026-03-16', timeStart: '10:00', timeEnd: '11:00'},
-    { id: 7, username: '2026002', fullName: 'Marlon Brant', subject: 'Science' , sched: '2026-03-16', timeStart: '09:00', timeEnd: '10:00'},
+    { id: 6, username: '2026003', fullName: 'Albert Concio', subject: 'English' , sched: '2026-03-16', timeStart: '11:00', timeEnd: '12:00'},
+    { id: 7, username: '2026002', fullName: 'Marlon Brant', subject: 'Science' , sched: '2026-03-16', timeStart: '11:00', timeEnd: '12:00'},
+    { id: 8, username: '2026004', fullName: 'James Smith', subject: 'Science' , sched: '2026-03-16', timeStart: '11:00', timeEnd: '12:00'},
+    { id: 9, username: '2026005', fullName: 'Charice Almond', subject: 'English' , sched: '2026-03-16', timeStart: '11:00', timeEnd: '12:00'},
+    { id: 10, username: '2026006', fullName: 'Anne Jona', subject: 'English' , sched: '2026-03-16', timeStart: '11:00', timeEnd: '12:00'},
+    { id: 11, username: '2026007', fullName: 'Ben Kong', subject: 'Science' , sched: '2026-03-15', timeStart: '22:00', timeEnd: '23:00'},
 ];
 
 module.exports = students;
--- data/users.js
+++ data/users.js
@@ -1,11 +1,12 @@
 const users = [
     { id: 1, username: 'admin', password: '1', isFaculty: 1, isAdmin: 1 },
-    { id: 2, username: '2026001', password: '1', isFaculty: 1, isAdmin: 1 },
+    { id: 2, username: '2026001', password: '1', isFaculty: 0, isAdmin: 0 },
     { id: 3, username: '2026002', password: '1', isFaculty: 0, isAdmin: 0 },
     { id: 4, username: '2026003', password: '1', isFaculty: 0, isAdmin: 0 },
     { id: 5, username: '2026004', password: '1', isFaculty: 0, isAdmin: 0 },
     { id: 6, username: '2026005', password: '1', isFaculty: 0, isAdmin: 0 },
     { id: 7, username: '2026006', password: '1', isFaculty: 0, isAdmin: 0 },
+    { id: 8, username: '2026007', password: '1', isFaculty: 0, isAdmin: 0 },
 ];
 
 module.exports = users;
